e7add4d0c12e2cddffae62041009ad9b2543aa14
Sheetal Bedarkar <sheetalbedarkar96@gmail.com>
2019-11-16 13:10:32 +0530

    Player checks for option No play, Ladder or Snake

--- SnakeAndLadderSimulator.sh
+++ SnakeAndLadderSimulator.sh
@@ -4,8 +4,23 @@ position=$START_POSITION
 rollsTheDie()
 {
 	randomCheckForDie=$((RANDOM%6+1))
-	echo $randomCheckForDie
-	position=$(($position+$randomCheckForDie))
-	echo $position
 }
-rollsTheDie
+
+randomCheckForOption=$((RANDOM%3))
+if [ $randomCheckForOption -eq 0 ]
+then
+	message="No play- The player stays in the same position"
+	position=$position
+
+elif [ $randomCheckForOption -eq 1 ]
+then
+	message="Ladder- The player moves ahead by the number of position"
+	rollsTheDie
+	position=$(($position+$randomCheckForDie))
+
+elif [ $randomCheckForOption -eq 2 ]
+then
+	message="Snake- The player moves behind by the number of position"
+	rollsTheDie
+	position=$(($position-$randomCheckForDie))
+fi
